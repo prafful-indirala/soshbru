27bb4d0a85e2ce1ec5c18782ae7f4e4e5c3b5879
Sai Prafful Indirala <prafful@builtbypixel.com>
2025-02-12 14:04:56 +0000

    screen linking changes

--- src/app/(tabs)/home/index.tsx
+++ src/app/(tabs)/home/index.tsx
@@ -1,30 +1,3 @@
-import React from 'react';
+import HomeScreen from '@/screens/HomeScreen';
 
-import useAuth from '@/hooks/useAuth';
-import Layout from '@/components/Layout';
-
-import { Button, ButtonText } from '@/ui/button';
-import { Text } from '@/ui/text';
-import { VStack } from '@/ui/vstack';
-
-export default function HomeScreen() {
-  const { logout } = useAuth();
-
-  return (
-    <Layout>
-      <VStack className="content-center items-center justify-center gap-[8px]">
-        <Text>Home Screen</Text>
-        <Button
-          size="md"
-          variant="solid"
-          action="primary"
-          isDisabled={false}
-          isFocusVisible={false}
-          onPress={logout}
-        >
-          <ButtonText>Logout</ButtonText>
-        </Button>
-      </VStack>
-    </Layout>
-  );
-}
+export default HomeScreen;
--- src/app/(tabs)/search/index.tsx
+++ src/app/(tabs)/search/index.tsx
@@ -1,21 +1,3 @@
-import React from 'react';
-import { SearchIcon } from 'lucide-react-native';
+import SearchScreen from '@/screens/SearchScreen';
 
-import Layout from '@/components/Layout';
-import { HStack } from '@/components/ui/hstack';
-import { Text } from '@/components/ui/text';
-
-import { VStack } from '@/ui/vstack';
-
-export default function SearchScreen() {
-  return (
-    <Layout>
-      <VStack className="gap-16">
-        <HStack className="items-center gap-2 self-center p-10">
-          <SearchIcon size={30} color="orange" />
-          <Text>Search Feature coming soon...</Text>
-        </HStack>
-      </VStack>
-    </Layout>
-  );
-}
+export default SearchScreen;
